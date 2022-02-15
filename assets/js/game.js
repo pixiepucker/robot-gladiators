16c07a4ca4433ff053fc94b50a55f8e12d322b85
pixiepucker <autumnoffic@gmail.com>
2022-02-15 17:21:01 -0600

    initial game setup

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,8 +1,9 @@
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
+var playerMoney = 10;
 
-//You can also log multiple values at tonce like this
+//You can also log multiple values at once like this
 console.log(playerName, playerAttack, playerHealth);
 
 var enemyName = "Roberto";
@@ -13,37 +14,62 @@ var enemyAttack = 12;
 var fight = function() {
     //alert players to round start
     window.alert("Welcome to Robot Gladiators!");
-
-    //subtract playerAttack value from enemyHealth to update the enemyHealth variable value
-    enemyHealth = enemyHealth - playerAttack;
-
-    //log a resulting message to console so we know that worked
-    console.log (
-    playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
-    );
-
-    //check enemy's health
-    if (enemyHealth <= 0) {
-        window.alert(enemyName + " has died!");
-    }
-    else {
-        window.alert(enemyName + " still has " + enemyHealth + " healthy left.");
-    }
-
-    //subtract enemyAttack value from playerHealth to update the playerHealth variable value
-    playerHealth = playerHealth - enemyAttack;
     
-    //log a resulting message to console so we know that worked
-    console.log (
-        enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
-    );
+    //added prompt to see if player would like to fight or skip round
+    var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
 
-    //check player's health
-    if (playerHealth <=0) {
-        window.alert(playerName + " has died!");
+    //log a result message to console so we know that worked
+    //console.log(promptFight);
+
+    //if player choses to fight, then fight
+    if (promptFight === "fight" || promptFight == "FIGHT") {
+
+        //subtract playerAttack value from enemyHealth to update the enemyHealth variable value
+        enemyHealth = enemyHealth - playerAttack;
+
+        //log a resulting message to console so we know that worked
+        console.log (
+            playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+        );
+
+        //check enemy's health
+        if (enemyHealth <= 0) {
+            window.alert(enemyName + " has died!");
+        } else {
+            window.alert(enemyName + " still has " + enemyHealth + " healthy left.");
+        }
+
+        //subtract enemyAttack value from playerHealth to update the playerHealth variable value
+        playerHealth = playerHealth - enemyAttack;
+        //log a resulting message to console so we know that worked
+        console.log (
+            enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+        );
+
+        //check player's health
+        if (playerHealth <=0) {
+            window.alert(playerName + " has died!");
+        } else {
+            window.alert(playerName + " still has " + playerHealth + " health left.");
+        }
+    //if player choses to skip
+    } else if (promptFight === "skip" || promptFight === "SKIP") {
+        //confirm player skip
+        var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+
+        //if yes (true), leave fight
+        if (confirmSkip) {
+        window.alert(playerName + " has decided to skip this fight. Goodbye!");
+        //subtract money from playerMoney for skipping
+        playerMoney = playerMoney - 2;
     }
+    //if no (false), ask question again by running fight() again
     else {
-        window.alert(playerName + " still has " + playerHealth + " health left.");
+        fight();
+    }
+    //if player didn't chose fight or skip
+    } else {
+        window.alert("You need to pick a valid option. Try again!");
     }
 };
 
