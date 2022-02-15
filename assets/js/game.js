2c2eb9ff213ab9189d274ddfff17ad35c64db863
pixiepucker <autumnoffic@gmail.com>
2022-02-15 01:58:39 -0600

    added our first variable with player input and a console.log to print that var

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,18 @@
-window.alert('This is an alert! JavaScript is running!');
+//this is a variable with is assigned thru player's input using the = operator
+var playerName = window.prompt('What is your robots name?')
+//This will print the value of var playerName in our developer console
+console.log(playerName);
+console.log('This logs a string, good for leaving yourself a message');
+//this will do math and log 20:
+console.log(10 + 10);
+//this next line will display the string + our var playerName input within our dev console
+console.log("Our robot's name is " + playerName);
+
+
+
+//this creates a function name fight:
+function fight() {
+    window.alert('The fight has begun!');
+}
+
+// fight();
