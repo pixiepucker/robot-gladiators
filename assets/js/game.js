598137dcd9b988ddd13198a4ff770091e603979f
pixiepucker <autumnoffic@gmail.com>
2022-02-17 00:51:00 -0600

    start and end game features added

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,8 +1,3 @@
-//GAME STATES
-//"WIN" - Player robot has defeated al enemy robots
-//  * Fight all enemy robots
-//  * Defeat each enemy robot
-//"LOSE" - Player robot's health is zero or less
 
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
@@ -74,16 +69,45 @@ var fight = function(enemyName) {
         }
     };
 
-//execute function
-for(var i = 0; i < enemyNames.length; i++) {
+//function to start new game
+var startGame = function(){
+    //reset player stats
+    playerHealth = 100;
+    playerAttack = 10;
+    playerMoney = 10;
+
+    for(var i = 0; i < enemyNames.length; i++) {
+        if (playerHealth > 0) {
+            window.alert("Welcome to Robot Gladiators! Round " + ( i + 1 ) );
+            var pickedEnemyName = enemyNames[i];
+            enemyHealth = 50;
+            fight(pickedEnemyName);
+        } else {
+            window.alert("You have lost your robot in battle! Game Over!");
+            break;
+        }
+    }
+    endGame();
+};
+
+//function to end entire game
+var endGame = function() {
+    //if player is still alive, player wins!!
     if (playerHealth > 0) {
-        window.alert("Welcome to Robot Gladiators! Round " + ( i + 1 ) );
-        var pickedEnemyName = enemyNames[i];
-        enemyHealth = 50;
-        debugger;
-        fight(pickedEnemyName);
+        window.alert("Great job, you've survived the game! You now have a score of " + playerMoney + ".");
     } else {
-        window.alert("You have lost your robot in battle! Game Over!");
-        break;
+        window.alert("You've lost your robot in battle :(");
+    }
+
+    var playAgainConfirm = window.confirm("Would you like to play again?");
+
+    if (playAgainConfirm) {
+        //restart game
+        startGame();
+    } else {
+        window.alert("Thank you for playing Robot Gladiators! Come back soon <3");
     }
 }
+
+//start game when page loads pls!
+startGame();
